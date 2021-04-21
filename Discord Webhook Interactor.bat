4525f2184ad2acfc131e6774c7ad85075aefa08f
elfy <75458737+xoElf@users.noreply.github.com>
2021-04-21 18:48:48 -0500

    Any text entered into the console will be sent to the Discord Webhook!

--- Discord Webhook Interactor.bat
+++ Discord Webhook Interactor.bat
@@ -1,6 +1,9 @@
 @echo off
 color F0
 title Batch Webhook Interactor
-set webhook=https://discord.com/api/webhooks/833895085921665045/jNHKUmbkSY_6VM4Hmk9B56BOxeEYBY7smXlGbEG050OSCJ2GhkJAj9DBaZgOpVBMPUVQ
-curl -X POST -H "Content-type: application/json" --data "{\"content\": \"`PC USERNAME: %USERNAME%`\n`TIME: %TIME%`\n`PUT ANYTHING HERE`\"}" %webhook% >nul
-exit
+echo MADE BY XOELF! CREDIT IF USED!
+set webhook=WEBHOOK_HERE
+:start
+set /p text=
+curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%text%\"}" %webhook% >nul
+goto start
