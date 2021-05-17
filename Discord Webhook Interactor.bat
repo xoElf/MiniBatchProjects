8568b7fefac5941f534f460a5972ec9dd1ac521e
elfy <75458737+xoElf@users.noreply.github.com>
2021-05-17 14:59:18 -0400

    Now allows  you to set a profile pic and name for the webhook!

--- Discord Webhook Interactor.bat
+++ Discord Webhook Interactor.bat
@@ -1,9 +1,13 @@
 @echo off
 color F0
 title Batch Webhook Interactor
+
+set webhook=https://discord.com/api/webhooks/839654307783114752/HS55yPPobKMeMbyE97FilP3jGh2B93ExX3iPCP40DnmRg7e-XFOlzcaZsbaeHPEOLyWO
+set name=BatchBot
+set pic=https://cdn.discordapp.com/attachments/839654283830099968/843919159641112606/hqdefault.jpg
+
 echo MADE BY XOELF! CREDIT IF USED!
-set webhook=WEBHOOK_HERE
-:start
-set /p text=
-curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%text%\"}" %webhook% >nul
-goto start
+:repeat
+set /p message=
+curl -H "Content-Type: application/json" -d "{\"username\": \"%name%\", \"content\":\"%message%\", \"avatar_url\":\"%pic%\"}" %webhook%
+goto repeat
